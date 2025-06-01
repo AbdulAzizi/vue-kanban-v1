fc0c87585b18752106ddc68eb5780ed023ce3e06
Azizjon Nurov <nurovaziz@gmail.com>
2025-06-02 03:46:03 +0500

    Introduce dragStore

---
+++ src/stores/dragStore.ts
@@ -0,0 +1,25 @@
+import { ref } from 'vue';
+import { defineStore } from 'pinia';
+import type { KanbanCardType, KanbanColumnType } from '@/types/kanbanTypes';
+
+export const useDragStore = defineStore('drag', () => {
+    const dragType = ref<null | 'column' | 'card'>(null);
+    const dragData = ref<null | KanbanColumnType | KanbanCardType>(null);
+
+    function startDrag(type: 'column' | 'card', data: KanbanColumnType | KanbanCardType) {
+        dragType.value = type;
+        dragData.value = data;
+    }
+
+    function clearDrag() {
+        dragType.value = null;
+        dragData.value = null;
+    }
+
+    return {
+        dragType,
+        dragData,
+        startDrag,
+        clearDrag,
+    };
+});
